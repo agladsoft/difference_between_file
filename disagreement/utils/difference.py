5cda304fc75a31558c096347257faae412ad7dac
timur <timurka.zavyalov@mail.ru>
2023-05-09 09:29:31 +0300

    add: added changes to the handling of differences

--- disagreement/utils/difference.py
+++ disagreement/utils/difference.py
@@ -1,21 +1,51 @@
 import difflib
 import io
 import re
-
+import logging
 from docx import Document
 from docx.enum.text import WD_COLOR_INDEX
 from docx.shared import Inches
 
 from disagreement.acceptable import replacements, skips
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('Documents')
+
+
+def list_unpacking(paragraphs):
+    document = []
+    logger.info('Delete duplicates')
+    for n, paragraph in enumerate(paragraphs):
+        result = [i for i in re.findall('(\.*,?\d{1,2}){1,4}\.?,? ?', paragraph) if '.' in i and 6 < paragraph.index(i)]
+        for i in sorted(result, reverse=True):
+            number = ''.join(
+                [i for i in paragraph[paragraph.index(i) - 4:paragraph.index(i) + 6] if i == '.' or i.isdigit()]).strip(
+                '.')
+            try:
+                start_index = paragraph.index(number)
+                if ''.join([paragraph[start_index - 3], paragraph[start_index - 2],
+                            paragraph[start_index - 1]]).lower().strip() == 'п.':
+                    continue
+                paragraphs[n] = paragraph[:start_index].strip()
+                document.append((n + 1, paragraph[start_index:].strip()))
+            except Exception:
+                ...
+
+    for i in sorted(document, key=lambda x: x[0], reverse=True):
+        paragraphs.insert(i[0], i[1])
+    return paragraphs
+
 
 def list_from_string(document: str) -> list:
     paragraphs = ['']
+    logger.info('Read file')
     for paragraph in document.split("\n"):
         if re.search('^(\.?,?\d{1,2}){1,4}\.?,? ?', paragraph):
-            paragraphs.append(re.sub(r"\ {2,}", " ", paragraph).replace("\xa0", " ").strip())
+            paragraphs.append(
+                re.sub(r"\ {2,}", " ", paragraph).replace("\xa0", " ").strip())
         else:
             paragraphs[-1] += re.sub(r"\ {2,}", " ", paragraph).replace("\xa0", " ").strip()
+    paragraphs = list_unpacking(paragraphs)
     return paragraphs
 
 
@@ -25,7 +55,7 @@ def get_diff(list1: list, list2: list) -> list:
     current_flag = None
     first_column = ""
     second_column = ""
-
+    logger.info('Find error')
     for diff in diff_items:
         if diff[0] == "-":
             if current_flag:
@@ -43,7 +73,28 @@ def get_diff(list1: list, list2: list) -> list:
     return diffs
 
 
+def filter_diffs(diffs):
+    duplicate = []
+    for num, diff in enumerate(diffs):
+        if not diff[0]:
+            result = diff[1][:7]
+            for n, dif in enumerate(diffs[num:]):
+                number = n + num
+                if result == dif[0][:7]:
+                    duplicate.append((num, number, (diffs[num][1], diffs[number][0])))
+                    break
+
+    duplicate.reverse()
+
+    for i in duplicate:
+        diffs.pop(i[1])
+        diffs[i[0]] = i[2]
+
+    return diffs
+
+
 def save_disagreement(file1: str, file2: str, count_error: int) -> io.BytesIO:
+    logger.info('Create document')
     result = Document()
     result.add_heading("Протокол разногласий")
     table = result.add_table(rows=1, cols=3)
@@ -58,13 +109,15 @@ def save_disagreement(file1: str, file2: str, count_error: int) -> io.BytesIO:
     heading_cells[2].width = Inches(3)
 
     list1, list2 = list_from_string(file1), list_from_string(file2)
-    #print(list1, list2)
     diffs = get_diff(list1, list2)
+    diffs = filter_diffs(diffs)
     for diff in diffs:
         match_number = re.search(r"^(\.?,?\d{0,2}){0,4} ?", diff[0])
         number = match_number[0] if match_number else ""
+        if not number:
+            match_number = re.search(r"^(\.?,?\d{0,2}){0,4} ?", diff[1])
+            number = match_number[0] if match_number else ""
         text1, text2 = [re.sub(r"(?:^(\.?,?\d{0,2}){0,4} ?|\.?,?$)", "", text).strip() for text in diff]
-
         cells = table.add_row().cells
         cells[0].width = Inches(0.6)
         cells[1].width = Inches(3)
@@ -73,12 +126,12 @@ def save_disagreement(file1: str, file2: str, count_error: int) -> io.BytesIO:
 
         left_diff_count = 0
         right_diff_count = 0
-
         sequence = difflib.SequenceMatcher(a=text1.lower(), b=text2.lower(), autojunk=False)
 
         paragraph1 = cells[1].paragraphs[0]
         paragraph2 = cells[2].paragraphs[0]
 
+        logger.info('Write document')
         for op, i1, i2, j1, j2 in sequence.get_opcodes():
             run1 = paragraph1.add_run(text1[i1:i2])
             run2 = paragraph2.add_run(text2[j1:j2])
@@ -91,8 +144,8 @@ def save_disagreement(file1: str, file2: str, count_error: int) -> io.BytesIO:
                     right_diff_count += j2 - j1
             if op == "replace":
                 if (text1[i1:i2], text2[j1:j2]) not in replacements and (
-                    text2[j1:j2],
-                    text1[i1:i2],
+                        text2[j1:j2],
+                        text1[i1:i2],
                 ) not in replacements:
                     run1.font.highlight_color = WD_COLOR_INDEX.YELLOW
                     run2.font.highlight_color = WD_COLOR_INDEX.YELLOW
@@ -103,7 +156,7 @@ def save_disagreement(file1: str, file2: str, count_error: int) -> io.BytesIO:
             table._tbl.remove(table.rows[-1]._tr)
 
     file_stream = io.BytesIO()
-    result.save(file_stream)
+    result.save('file_stream.docx')
     file_stream.seek(0)
 
     return file_stream
